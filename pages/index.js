ffbf585b34103e2651a6b9daf1f630129251153b
Carolina <carolinapcarvalho97@gmail.com>
2025-09-26 18:55:51 -0300

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Amor, se você estiver vendo isso, coloca água nas plantas, por favorzinho! Te amo e te gosto</h1>
+  return (
+    <h1>
+      Amor, se você estiver vendo isso, coloca água nas plantas, por favorzinho!
+      Te amo e te gosto
+    </h1>
+  );
 }
 
 export default Home;
